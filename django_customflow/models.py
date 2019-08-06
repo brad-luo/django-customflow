dfd09478a50febee8e708e79f176b18c6d2a49ac
luoxiaole <luoxiaole@globalegrow.com>
2019-08-06 17:23:48 +0800

    upload some basic files/informations.

---
+++ django_customflow/models.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+# create_time: 2019/8/6 16:42
+# __author__ = 'brad'
+
--- setup.py
+++ setup.py
@@ -2,3 +2,21 @@
 # create_time: 2019/8/6 15:30
 # __author__ = 'brad'
 
+
+from setuptools import setup
+
+setup(
+    name='django-customflow',
+    version='0.1.0',
+    description='A custom workflow with Django',
+    url='https://github.com/Brad19940809/django-customflow',
+    author='Brad',
+    author_email='xiaoleluo2@gmail.com',
+    maintainer="Brad",
+    maintainer_email='xiaoleluo2@gmail.com',
+    license="MIT",
+    keywords='workflow, django, flow',
+    install_requires=[
+        'django'
+    ],
+)
